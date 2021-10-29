e53a2488192811c35fa82923d5cc6702f458db5f
Dark-Slide <martin.haagendrup@hotmail.com>
2021-10-29 14:06:22 +0200

    Fixed Login and CardGame

---
+++ Introduktion/CardGame/CardGame/Program.cs
@@ -0,0 +1,10 @@
+﻿using CardGame;
+//Instantierer vi et Game object og kører metoden start
+//direkte uden reference til objektet
+new Game().Start();
+
+//Instantierer vi et Game object med reference (g)
+//Efterfølgende kører vi metoden Start() via referencen
+
+//Game g = new Game();
+//g.Start();
---
+++ Introduktion/Login System/Login System/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Login_System;
+using static Login_System.UserData;
+
+namespace Login_System // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // Initial Program Load
+
+            // Variables
+            string UsernameInput, PasswordInput;
+            int UserID;
+
+
+            // Welcome Message
+            Console.WriteLine("Welcome. To continue, you must enter your username and password: \n\n");
+
+
+            // Ask for Username Input
+
+            // Send UsernameInput to Checker Method
+            while (true)
+            {
+
+                Console.WriteLine("Please Enter your username!");
+
+                UsernameInput = Console.ReadLine().ToString();
+
+                UserID = User.UsernameMatch(UsernameInput);
+
+                if (User.UsernameMatch(UsernameInput) != 404)
+                {
+
+                }
+            }
+
+
+            // Ask for Password Input
+
+            Console.WriteLine($"Hello {UsernameInput} please enter your Password:");
+        }
+    }
+}
